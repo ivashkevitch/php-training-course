a069b98c657b9a3ae8b6bc19908d4043d0b5f2b1
Artyom Ivashkevich <artyom.ivashkevich@gmail.com>
2018-04-03 22:30:20 +0700

    File uploader

---
+++ www/upload.php
@@ -0,0 +1,39 @@
+<?php
+if (!empty($_FILES['attachment'])) {
+    $file = $_FILES['attachment'];
+
+    $srcFileName = $file['name'];
+    $newFilePath = __DIR__ . '/uploads/' . $srcFileName;
+
+    $allowedExtensions = ['jpg', 'png', 'gif'];
+    $extension = pathinfo($srcFileName, PATHINFO_EXTENSION);
+    if (!in_array($extension, $allowedExtensions)) {
+        $error = 'Загрузка файлов с таким расширением запрещена!';
+    } elseif ($file['error'] !== UPLOAD_ERR_OK) {
+        $error = 'Ошибка при загрузке файла.';
+    } elseif (file_exists($newFilePath)) {
+        $error = 'Файл с таким именем уже существует';
+    } elseif (!move_uploaded_file($file['tmp_name'], $newFilePath)) {
+        $error = 'Ошибка при загрузке файла';
+    } else {
+        $result = 'http://myproject.loc/uploads/' . $srcFileName;
+    }
+}
+?>
+<html>
+<head>
+    <title>Загрузка файла</title>
+</head>
+<body>
+<?php if (!empty($error)): ?>
+    <?= $error ?>
+<?php elseif (!empty($result)): ?>
+    <?= $result ?>
+<?php endif; ?>
+<br>
+<form action="/upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="attachment">
+    <input type="submit">
+</form>
+</body>
+</html>
